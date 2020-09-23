45e06cd62c55945b0ea88c34e7fb7f4baf1680c1
xCoreh <xCoreh@Corys-PC>
2020-09-22 20:13:42 -1000

    Initial

---
+++ HelloWorld/src/clp1b.java
@@ -0,0 +1,9 @@
+// Cory Parker, lab section 5
+public class clp1b {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ HelloWorld/src/clp1c.java
@@ -0,0 +1,10 @@
+// Cory Parker, lab section 5
+public class clp1c {
+
+	public static void main(String[] args) {
+		String name = ("Cory");
+		System.out.println("Hello " + name);
+
+	}
+
+}
