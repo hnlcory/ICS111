68f6ee9130d3529e5274713f506bd1a0ed2d1ca6
xCoreh <xCoreh@Corys-PC>
2020-10-04 13:54:07 -1000

    most done

--- Project6/src/AllSubstrings.java
+++ Project6/src/AllSubstrings.java
@@ -3,17 +3,46 @@ import java.util.Scanner;
 public class AllSubstrings {
 
 	public static void printSubstrings(String str) {
-		for (int i=0; i < str.length(); i++) {
-			System.out.println(str);
-		}
-		
+		int count=1;
+		 System.out.print("The substrings of "+str+" are ");
+         
+	        for (int i = 0; i < str.length(); i++) {
+	        	
+	            for (int c = i+1; c <= str.length(); c++) {
+	                System.out.print("\""+str.substring(i, c));
+	                count++;
+	                if (count==c) {
+	                	break;
+	                }
+	                else {
+	                	if (count+1==c) {
+	                		System.out.print("\"");
+	                		break;
+	                	}
+	                	else {
+	                		System.out.print("\"");
+	                		System.out.print(", ");
+	                	}
+	                }
+	            }
+	        }
 	}
 	public static void main(String[] args) {
 		System.out.println("enter a string:");
-		Scanner strI = new Scanner(System.in);
-		String strImp = strI.nextLine();
-		strI.close();
-		printSubstrings(strImp);
+		while (true) {
+			Scanner strI = new Scanner(System.in);
+			String strImp = strI.nextLine();
+			if (strImp.equals("done")) {
+				strI.close();
+				break;
+			}
+			else {
+				printSubstrings(strImp);
+			}
+		}
+			
+		
+		
 
 	}
 
