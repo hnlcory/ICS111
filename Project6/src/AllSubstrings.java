4cab607bb4019618275694d5f262ec4a460618c5
hnlcory <hnlcory@localhost>
2020-10-08 18:50:43 -1000

    final + comments

--- Project6/src/AllSubstrings.java
+++ Project6/src/AllSubstrings.java
@@ -4,30 +4,28 @@ public class AllSubstrings {
 
 	public static void printSubstrings(String str) {
 		int count=1;
-		 System.out.print("The substrings of "+str+" are ");
+		 System.out.print("The substrings of "+str+" are ");//print structure
          
-	        for (int i = 0; i < str.length(); i++) {
+	        for (int i = 0; i < str.length(); i++) {// iteration of loop
 	        	
 	            for (int c = i+1; c <= str.length(); c++) {
 	                System.out.print("\""+str.substring(i, c));//substring start, end spot
-	                	if (i+1== str.length()) {
+	                	if (i+1== str.length()) {// if the iteration is the final character print the last "
 	                		System.out.print("\"");
 	                		break;
 	                	}
 	                	else {
 	                		System.out.print("\"");
-	                		System.out.print(", ");
-	                	}
-	                
-	            }
-	            
+	                		System.out.print(", ");// prints the ", for every substring
+	                	}    
+	            }            
 	        }
 	        System.out.println(" ");
 	}
 	public static void main(String[] args) {
 		Scanner strI = new Scanner(System.in);
 		
-		while (true) {
+		while (true) {//infinate loop to be broken by if statement (done)
 			System.out.print("enter a string:");
 			String strImp = strI.nextLine();
 			if (strImp.equals("done")) {
