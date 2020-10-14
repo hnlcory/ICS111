2216250a1b5c0d9a7e73b31d8bb267f8d382868d
hnlcory <hnlcory@localhost>
2020-10-14 10:32:50 -1000

    messing with need for recursion

--- Project7/src/PrintHex.java
+++ Project7/src/PrintHex.java
@@ -4,23 +4,31 @@ import java.util.Scanner;
 public class PrintHex {
 
 	public static String intToHex(int n) {
+		String f = Integer.toHexString(n);
+		System.out.println(f);
+		System.out.println("enter a number: ");
+		Scanner A= new Scanner(System.in);
+		
+		
+		if () {
+		break;
+		}
+		else {
+		intToHex();	
+		}
+		return f;
 		
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("enter a number: ");
 		Scanner numI = new Scanner(System.in);
-		while (true) {
-			int numImp = numI.nextInt();
-			if (numImp <= -1) {
-				numI.close();
-				break;
-			}
-			else {
-				intToHex(numImp);
-			}
+		int numImp = numI.nextInt();
+		numI.close();	
+		intToHex(numImp);
 			
-		}
+			
+		
 
 	}
 
