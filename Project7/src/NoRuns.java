541d52022e607fda15adcbddc96950f30e787521
xCoreh <xCoreh@Corys-PC>
2020-10-15 12:05:01 -1000

    final with comments

--- Project7/src/NoRuns.java
+++ Project7/src/NoRuns.java
@@ -5,26 +5,26 @@ import java.util.Arrays;
 public class NoRuns {
 
 	public static void main(String[] args) {
-		String[] array1=args;
+		String[] array1=args;//array from args
 		int count=0;
-		//System.out.println(Arrays.toString(array1));
-		ArrayList<String> list= new ArrayList<String>(Arrays.asList(array1));
-		//System.out.println(list);
-		while(count<list.size()) {
 		
-			for(int i=0;i<list.size()-1;i++) {
+		ArrayList<String> list= new ArrayList<String>(Arrays.asList(array1));//create array list from array
+		
+		while(count<list.size()) {//run for size of list
+		
+			for(int i=0;i<list.size()-1;i++) {//for each duplicate word next to each other remove duplicate
 				if (list.get(i).equals(list.get(i+1))) {
 					list.remove(i);
-					//System.out.println(list);
+					
 				}
 			}
 			count++;
 		}
 		
-		String formattedString = list.toString().replace("]", " ").replace("[", " ");
+		String formattedString = list.toString().replace("]", " ").replace("[", " ");//format output to get rid of the [ and ]
 		
 		int size = list.size();
-		System.out.print("the array now contains the "+size+" element(s):"+formattedString);
+		System.out.print("the array now contains the "+size+" element(s):"+formattedString);//final printout
 		
 	}
 }
