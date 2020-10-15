1e99e49cd03a0b041e4bc6b3241c748d787fe4c9
xCoreh <xCoreh@Corys-PC>
2020-10-14 20:11:37 -1000

    close to final

--- Project7/src/PrintHex.java
+++ Project7/src/PrintHex.java
@@ -2,34 +2,30 @@
 import java.util.Scanner;
 
 public class PrintHex {
-
-	public static String intToHex(int n) {
-		String f = Integer.toHexString(n);
-		System.out.println(f);
-		System.out.println("enter a number: ");
-		Scanner A= new Scanner(System.in);
-		
-		
-		if () {
-		break;
-		}
-		else {
-		intToHex();	
-		}
-		return f;
-		
+	
+	public static void DecimalToHexa() {
+		 char[] hexa= {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+		 Scanner numImp = new Scanner(System.in);
+		 int num = numImp.nextInt();
+		 int rem=0;
+		 String hexOut="";
+		   
+		 if(num <0) {
+			 numImp.close();
+			 return;
+		 }
+		 
+		 while (num>0) {
+			 rem=num%16;
+			 hexOut=hexa[rem]+hexOut;
+			 num=num/16;
+		 }
+		 System.out.println(hexOut);
+		 DecimalToHexa();
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("enter a number: ");
-		Scanner numI = new Scanner(System.in);
-		int numImp = numI.nextInt();
-		numI.close();	
-		intToHex(numImp);
-			
-			
-		
-
-	}
-
+		DecimalToHexa();
+   
+  }
 }
