98a7e5984828087c370bad5f07fac9edc19700ad
xCoreh <xCoreh@Corys-PC>
2020-10-27 17:48:14 -1000

    test files + 9B start

--- Project9/src/Median.java
+++ Project9/src/Median.java
@@ -1,40 +1,59 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 //Cory Parker - Section 5
 public class Median {
 
+	public static ArrayList<Integer> arrList = new ArrayList<Integer>();
+	
 	
 		
 	public static void main(String[] args) {
 		int argcount=0;
-		if(args.length==0) {
+		if(args.length==0) {//args check
 			System.out.println("no args provided");//check for args
 			 System.exit(1);
 		}
-		while(argcount <args.length) {
+		while(argcount <args.length) {//for each file
 			FileReader fread=null;
 			File file = new File(args[argcount]);
+			Scanner s = null;
 			
 			
-			try {
+			try {//file read and open
 				fread= new FileReader(file);
+				s = new Scanner(fread);
 			}
 			catch(FileNotFoundException e){
 				System.out.println("file not found");
 				System.exit(1);
 			}
 			
+			//check for file with anything except , and integers
+			
+			while (s.hasNext()) {//parse int into arraylist
+				String curLine = s.nextLine();
+				System.out.println(curLine);
+				String [] values=curLine.split(",");
+				for(String value: values) {
+					System.out.println(Integer.parseInt(value));
+					arrList.add(Integer.parseInt(value));
+				}
+				
+			}
+			System.out.println(arrList);
+			
 		
-		
-		//read file  args
-		//parse as integers to arraylist
+		//read file  args #
+		//parse as integers to arraylist #
 		//compute the median value - has to use specific algorithm - 1 or more variables to keep track of whether you have already seen one of the two numbers whose average is the mean
-		// handle - no file, no separating comma integers, any others - print message, go to next file
+		// handle - no file#, no args#, no separating comma integers, any others - print message, go to next file
 		
-		
-		}
+			argcount++;
+		}//end of while loop
 	}
 
 }
