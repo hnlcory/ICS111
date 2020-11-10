ba1a2fcc594340dd9a4473d84f0db294dcf97964
xCoreh <xCoreh@Corys-PC>
2020-11-09 20:20:50 -1000

    updates

--- Project11/src/ContactP.java
+++ Project11/src/ContactP.java
@@ -1,4 +1,9 @@
 
-public class ContactP {
+public class ContactP extends Person {
 
+	public String toString(){//not sure if correct usage of super
+		super.toString();
+		return super.toString();
+	}
+	
 }
